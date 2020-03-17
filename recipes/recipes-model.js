c59ed1cb9bd77c64fa36c26f8448581b27a6027c
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-03-17 17:01:04 +0000

    added first helper and route

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -1,5 +1,20 @@
-// - `getRecipes()`: should return a list of all recipes in the database.
 // - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
 // - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
 
+// STRETCH
+// - `GET /api/recipes/:id/instructions`: a correctly ordered list of how to prepare a single recipe
+// - `GET /api/ingredients/:id/recipes`: all recipes in the system that utilize a single ingredient
 const db = require('../db-config')
+
+
+module.exports = {
+    getRecipes,
+    
+  };
+
+  //GETS all recipes
+
+  function getRecipes() {
+      return db("recipes");
+
+  }
---
+++ recipes/recipes-router.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+
+// -->Embed other routers in here!!!! like in guided project 3
+// e.g `GET /api/recipes/:id/shoppingList`: getShoppingList
+
+
+const recipes = require("./recipes-model");
+const router = express.Router();
+
+
+//GET  '/' --> GETS ALL RECIPES 
+
+router.get("/", async (req, res, next) => {
+    try {
+      res.json(await recipes.getRecipes());
+    } catch (error) {
+      next(error);
+    }
+  });
+
+
+
+
+
+module.exports = router;
